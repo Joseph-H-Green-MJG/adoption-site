5b2f5b29ffdf920de4acb408fea15a04f4cdfd5c
Joseph <thecaliman@gmail.com>
2020-10-16 15:23:42 -0700

    added react-route and updated site name, and favicon

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Component1 from "./pages/component1"; 
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux</h2>
+      <div className='container'>
+        <Router>
+          <div>
+              <Switch>
+                <Route path="/component1" component={Component1} />
+              </Switch>
+          </div>
+        </Router>
+
       </div>
     );
   }
---
+++ src/components/pages/component1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Component1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is a test</h1>
+            </div>
+        );
+    }
+}
+
+export default Component1;
